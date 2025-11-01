72c287a601bf748f2bd28fe64ffbdf6f6ebf2b6d
LeulAria <leulsegedtariku@gmail.com>
2025-11-02 01:02:06 +0400

    added nitro server

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,7 +3,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
-	plugins: [tsconfigPaths(), tailwindcss(), tanstackStart(), viteReact()],
+	plugins: [tsconfigPaths(), nitro(), tailwindcss(), tanstackStart(), viteReact()],
 });
